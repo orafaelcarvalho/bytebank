90ef54acca60cca5c015e9678db3006b438ad7d6
Rafael Souza de Carvalho <rsouzadecarvalho@gmail.com>
2022-11-22 09:51:37 -0300

    Subindo atividades realizadas

---
+++ bytebank/ContaCorrente.cs
@@ -0,0 +1,111 @@
+﻿using bytebank;
+using bytebank.Titular;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank
+{
+    public class ContaCorrente
+    {
+        public Cliente titular;
+        private string _conta;
+        public string Conta
+        {
+            get { return _conta; }
+            set
+            {
+                if (value != null)
+                    _conta = value;
+            }
+        }
+        private int _numeroAgencia;
+        public int NumeroAgencia
+        {
+            get { return _numeroAgencia; }
+            set
+            {
+                if (value > 0)
+                    _numeroAgencia = value;
+            }
+        }
+        public string NomeAgencia { get; set; }
+        private double _saldo;
+
+        public bool Sacar(double valor)
+        {
+            if (_saldo < valor || valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                _saldo = _saldo - valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public bool Tranferir(double valor, ContaCorrente destino)
+        {
+            if (_saldo < valor || valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                _saldo = _saldo - valor;
+                destino._saldo = destino._saldo + valor;
+                return true;
+            }
+        }
+
+        //public void SetSaldo(double valor)
+        //{
+        //    if(valor < 0)
+        //    {
+        //        return;
+        //    }
+        //    else
+        //    {
+        //        saldo += valor;
+        //    }
+        //}
+
+        //public double GetSaldo()
+        //{
+        //    return saldo;
+        //}
+
+        public double Saldo
+        {
+            get { return _saldo; }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                else
+                {
+                    _saldo = value;
+                }
+            }
+        }
+
+        public ContaCorrente(int numeroagencia, string conta)
+        {
+            NumeroAgencia = numeroagencia;
+            Conta = conta;
+            TotalDeContasCriadas += 1;
+        }
+
+        public static int TotalDeContasCriadas{ get; set; }
+    }
+}
---
+++ bytebank/Program.cs
@@ -0,0 +1,31 @@
+﻿using bytebank;
+using bytebank.Titular;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Boas vindas ao seu banco, Bytebank");
+
+        ContaCorrente conta1 = new ContaCorrente(1, "1234-X");
+        conta1.NomeAgencia = "Matriz";        
+
+        ContaCorrente conta2 = new ContaCorrente(1, "4321-Y");
+        //conta2.titular = "Roberto";
+        conta2.NomeAgencia = "Matriz";
+
+
+        conta1.Saldo=200;
+        conta2.Saldo=100;
+        Console.WriteLine($"Saldo 1: {conta1.Saldo}");
+        Console.WriteLine($"Saldo 2: {conta2.Saldo}");
+
+        conta1.Tranferir(50, conta2);
+
+        Console.WriteLine($"Saldo 1: {conta1.Saldo}");
+        Console.WriteLine($"Saldo 2: {conta2.Saldo}");
+
+        Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+        Console.ReadKey();
+    }
+} 
